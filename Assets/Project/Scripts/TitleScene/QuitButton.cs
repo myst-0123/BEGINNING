79769a35d00d905561b5dc9761687b650faccf1f
myst-0123 <52592597+myst-0123@users.noreply.github.com>
2023-03-04 21:16:17 +0900

    Add TitleScene シーンの作成とボタンの配置・スクリプトを作成

---
+++ Assets/Project/Scripts/TitleScene/QuitButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TitleScene
+{
+    public class QuitButton : MonoBehaviour
+    {
+        public void OnClick()
+        {
+            Debug.Log("èIóπ Pushed");
+        }
+    }
+
+}
