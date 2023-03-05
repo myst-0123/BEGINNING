8d4db92447415ebb7edebd1e3c0d994fb946af91
myst-0123 <52592597+myst-0123@users.noreply.github.com>
2023-03-05 22:09:48 +0900

    Fix #3 アニメーションもっさり問題を解決 確認求む

--- Assets/Project/Scripts/Player_Run.cs
+++ Assets/Project/Scripts/Player_Run.cs
@@ -13,14 +13,13 @@ public class Player_Run : MonoBehaviour
 
     void Update()
     {
-        float horizontalKey = Input.GetAxis("Horizontal");
-        if (horizontalKey > 0) 
+        if (Input.GetKey(KeyCode.D))
         {
             transform.localScale = new Vector3(1, 1, 1);
             anim.SetBool("Run", true);
             transform.Translate(0.01f, 0, 0);
         }
-        else if (horizontalKey < 0)
+        else if (Input.GetKey(KeyCode.A))
         {
             transform.localScale = new Vector3(-1,1, 1);
             anim.SetBool("Run", true);
