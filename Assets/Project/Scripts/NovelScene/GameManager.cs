638179de29e01eeffe06b51f6bad6a13cc2aa5e9
myst-0123 <52592597+myst-0123@users.noreply.github.com>
2023-03-12 17:03:18 +0900

    Add FadeIn シーンが始めるときのフェードインを追加

--- Assets/Project/Scripts/NovelScene/GameManager.cs
+++ Assets/Project/Scripts/NovelScene/GameManager.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,11 +16,37 @@ namespace NovelScene
 
         public int lineNumber;
 
+        [SerializeField] Image _fadePanel;
+
+        private float _fadeTime = 1.0f;
+        private int _loopCount = 100;
+        private float _waitTime;
+        private float _interval;
+
         private void Awake()
         {
             Instance = this;
 
+            _waitTime = _fadeTime / _loopCount;
+            _interval = 255.0f / _loopCount;
+
             lineNumber = 0;
+
+            StartCoroutine("FadeIn");
+        }
+
+        private IEnumerator FadeIn()
+        {
+            _fadePanel.color = new Color(0, 0, 0, 1);
+
+            for (float a = 255.0f; a >= 0.0f; a -= _interval)
+            {
+                yield return new WaitForSeconds(_waitTime);
+
+                Color newColor = _fadePanel.color;
+                newColor.a = a / 255.0f;
+                _fadePanel.color = newColor;
+            }
         }
     }
 }
